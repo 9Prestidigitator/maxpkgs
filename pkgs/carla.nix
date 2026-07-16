a4e3ccac7c4a648b2017c7eb8305165f0cb507a1
9Prestidigitator <9prestidigitator@gmail.com>
2026-07-16 19:55:58 -0400

    Trying out carla fix

---
+++ pkgs/carla.nix
@@ -0,0 +1,12 @@
+{
+  carla,
+  which,
+}:
+carla.overrideAttrs (oldAttrs: {
+  postFixup = (oldAttrs.postFixup or "") + ''
+    wrapQtApp "$out/lib/carla/carla-bridge-lv2-modgui" \
+      --prefix PATH : "$program_PATH:${which}/bin" \
+      --prefix PYTHONPATH : "$program_PYTHONPATH" \
+      --set PYTHONNOUSERSITE true
+  '';
+})
