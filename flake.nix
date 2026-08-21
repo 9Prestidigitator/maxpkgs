65575d11358cf58b5d0a4cf744ce17f7dcc2b223
9Prestidigitator <9Prestidigitator@gmail.com>
2026-08-20 20:39:40 -0400

    updated flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     import-tree.url = "github:vic/import-tree";
 
-    pulse-visualizer.url = "github:Audio-Solutions/pulse-visualizer";
+    pulse-visualizer = {
+      url = "github:Audio-Solutions/pulse-visualizer";
+      inputs.flake-parts.follows = "flake-parts";
+    };
   };
 
   outputs = inputs:
